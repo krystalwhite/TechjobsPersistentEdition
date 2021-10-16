1f8549903122851373d9540cc04887f94d7e46d5
krystalwhite <krystalsnowhite@gmail.com>
2021-10-16 15:31:54 -0500

    completed skill section of part 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,45 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill(){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Skill skill = (Skill) o;
+        return Objects.equals(description, skill.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), description);
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "description='" + description + '\'' +
+                '}';
+    }
 }
