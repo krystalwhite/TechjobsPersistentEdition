6f8abe63901579dc8b32568093a40ae395940bda
krystalwhite <krystalsnowhite@gmail.com>
2021-10-17 20:46:49 -0500

    completed up to HomeController on part 3

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,10 +1,14 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -13,6 +17,10 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Location is a required field.")
     @Size(min=1, max = 100, message = "Maximum character length is 100.")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(String location) {
         this.location = location;
