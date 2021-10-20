4356b35784ea5b51f97db704365d0013f1e444da
krystalwhite <krystalsnowhite@gmail.com>
2021-10-19 21:04:35 -0500

    all tests in part 2 pass

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 --columns: int id, varchar(255) employer, varchar(255) name, varchar(255) skills
 
 -- Part 2: Test it with SQL
-SELECT name FROM employer WHERE location="St. Louis City";
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
