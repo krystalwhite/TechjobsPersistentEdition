1d6b1a2ff72c0ce3a7adbae6b2cc97651961b9b8
krystalwhite <krystalsnowhite@gmail.com>
2021-10-22 19:39:56 -0500

    part 3 completed

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -42,7 +42,6 @@ public class HomeController {
         model.addAttribute(new Job());
         model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute("skills", skillRepository.findAll());
-        model.addAttribute("jobs", jobRepository.findAll());
         return "add";
     }
 
