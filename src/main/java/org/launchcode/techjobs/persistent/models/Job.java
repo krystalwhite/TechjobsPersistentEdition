d02f58028295406221bc8d3a653a0a069130f311
krystalwhite <krystalsnowhite@gmail.com>
2021-10-22 20:16:41 -0500

    completed job.skills refactoring

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity {
@@ -8,21 +10,21 @@ public class Job extends AbstractEntity {
     @ManyToOne
     private Employer employer;
 
-//    should this be @ManyToMany??
-    private String skills;
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
+//    private String skills;
 
     public Job() {
     }
 
-    public Job(Employer anEmployer, String someSkills) {
+//    does job need to take in skills as a parameter? does it need to initialize skills?
+    public Job(Employer anEmployer, List<Skill> skills) {
         super();
         this.employer = anEmployer;
-        this.skills = someSkills;
+        this.skills = skills;
     }
 
     // Getters and setters
-
-
     public Employer getEmployer() {
         return employer;
     }
@@ -31,11 +33,11 @@ public class Job extends AbstractEntity {
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {
         this.skills = skills;
     }
 }
