f5cc0b2e915bd1db6856361780358414c97579a5
krystalwhite <krystalsnowhite@gmail.com>
2021-10-24 16:42:35 -0500

    back to working tests

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -59,9 +59,13 @@ public class HomeController {
         }
         Employer jobEmployer = employerRepository.findById(employerId).orElse(new Employer());
         newJob.setEmployer(jobEmployer);
+//        System.out.println("From webpage: " + jobEmployer.getLocation());
         List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+//        System.out.println("List: " + skillObjs.toArray().length);
         newJob.setSkills(skillObjs);
         jobRepository.save(newJob);
+//        skillObjs.add();
+//        skillRepository.save(skillObjs);
         return "redirect:";
     }
 
